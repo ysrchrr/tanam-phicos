22356120275089cff752bd52fc5b59f263b10556
niredocz <daffaaldzakian20@gmail.com>
2020-12-31 10:16:00 +0700

    update controller to load product from DB

--- app/Controllers/Front.php
+++ app/Controllers/Front.php
@@ -1,13 +1,20 @@
-<?php namespace App\Controllers;
+<?php 
 
-class Front extends BaseController
-{
-	public function index()
-	{
+namespace App\Controllers;
+use CodeIgniter\Controller;
+use App\Models\Front\Product_view;
+
+class Front extends BaseController {
+	public function index() {
+		$model = new Product_view();
 		$data = array(
-			'title' => 'Front - Sapphire'
+			'title' => 'Front - Sapphire',
+			'product' => $model->get_product_list()->getResult()
 		);
-		return view('front/index', $data);
+		// $data['title'] = 'Front - Sapphire part 2';
+		// $data['product'] = $model->get_product_list()->getResult();
+
+		echo view('front/index', $data);
 	}
 
 	public function test() 
