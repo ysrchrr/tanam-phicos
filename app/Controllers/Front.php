16799b644798743c9623b64c1f20f622c697345a
Roy Aldo <aldorezpector60@gmail.com>
2021-01-04 11:38:30 +0700

    table kategori

--- app/Controllers/Front.php
+++ app/Controllers/Front.php
@@ -1,11 +1,14 @@
-<?php 
+<?php
 
 namespace App\Controllers;
+
 use CodeIgniter\Controller;
 use App\Models\Front\Product_view;
 
-class Front extends BaseController {
-	public function index() {
+class Front extends BaseController
+{
+	public function index()
+	{
 		$model = new Product_view();
 		$data = array(
 			'title' => 'Front - Sapphire',
@@ -17,9 +20,19 @@ class Front extends BaseController {
 		echo view('front/index', $data);
 	}
 
-	public function test() 
+	public function test()
 	{
 		return view('referensi/front-e-commerce');
+	}
+
+	public function tampilkategori()
+	{
+
+	}
+
+	public function tampildata()
+	{
+		# code...
 	}
 	//--------------------------------------------------------------------
 
