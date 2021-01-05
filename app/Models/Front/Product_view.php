ae662bc18c121c9707e0612ba4ff6dbde8070378
niredocz <daffaaldzakian20@gmail.com>
2021-01-05 13:19:04 +0700

    update query for Product model

--- app/Models/Front/Product_view.php
+++ app/Models/Front/Product_view.php
@@ -6,22 +6,30 @@ use CodeIgniter\Model;
 
 class Product_view extends Model
 {
-    // SELECT * FROM `barang`
-    // LEFT JOIN gambar on gambar.id_barang = barang.id_barang
-    public function get_product_list($where)
+    public function get_product_list($kategori)
     {
-        $query = "select barang.*, gambar.link_gambar from barang left join gambar on gambar.id_barang = barang.id_barang ";
-
-        if (isset($where) && !empty($where)) {
-            $query .= "where id_kategori ='$where'";
-        }
-        return $this->query($query);
-    }
-    public function get_product_detail() {
         $data = $this->db->table('barang');
-        $data->select('barang.*, gambar.link_gambar');
+        $data->select('barang.*, gambar.link_gambar, kategori.nama_kategori');
         $data->join('gambar', 'gambar.id_barang = barang.id_barang', 'left');
-        // $data->where('barang.id_barang = ' $id);
+        $data->join('kategori', 'kategori.id_kategori = barang.id_kategori', 'left');
+
+        if (isset($kategori) && !empty($kategori)) {
+            $data->where("barang.id_kategori = '$kategori'");
+        }
+
+        return $data->get();     
+    }
+
+    public function get_product_detail($kategori, $id) {
+        $data = $this->db->table('barang');
+        $data->select('barang.*, gambar.link_gambar, kategori.nama_kategori');
+        $data->join('gambar', 'gambar.id_barang = barang.id_barang', 'left');
+        $data->join('kategori', 'kategori.id_kategori = barang.id_kategori', 'left');
+
+        if (isset($id) && !empty($id)) {
+            $data->where("barang.id_barang = '$id'");
+        }
+
         return $data->get();
     }
 }
