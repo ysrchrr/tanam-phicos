4dcde1580c304902f6eba8ee1625f5f74278f2ae
Roy Aldo <aldorezpector60@gmail.com>
2021-01-13 10:33:50 +0700

    db

---
+++ app/Views/front/pages/pengiriman.php
@@ -0,0 +1,11 @@
+<?= $this->extend('front/layout/home'); ?>
+<?= $this->section('content'); ?>
+<div class="row">
+    <div class="container">
+        <div class="col-md-12 box padding text-center">
+            <p class="box padding" style="font-weight: 500;font-size: 70px;"> COMING SOON</p>
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
